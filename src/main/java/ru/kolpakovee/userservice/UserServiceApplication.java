ef78f77cb31197026bd33399cea70528272e7661
kolpakovee <eekolpakov@edu.hse.ru>
2025-01-28 20:38:05 +0500

    Add resource-server dependency and check jwt token

--- src/main/java/ru/kolpakovee/userservice/UserServiceApplication.java
+++ src/main/java/ru/kolpakovee/userservice/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package ru.kolpakovee.user_service;
+package ru.kolpakovee.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/ru/kolpakovee/userservice/controllers/UserController.java
@@ -0,0 +1,15 @@
+package ru.kolpakovee.userservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello, authenticated user!";
+    }
+}
